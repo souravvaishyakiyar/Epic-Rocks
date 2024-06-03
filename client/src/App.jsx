9e8073984347d36581724bf020ea298ea628c5cc
Sourav <souravvaishyakiyar@gmail.com>
2024-06-03 17:03:11 +0530

    Create routes for the section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import Profile from './Pages/Profile'
+import Signup from './Pages/Signup'
+import About from './Pages/About'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+    </Routes>
+    </BrowserRouter>
+    
+  );
 }
 
 export default App
