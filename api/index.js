7779ac10cda44d3a0422cc02bace4fc5d91ea87c
Sourav <souravvaishyakiyar@gmail.com>
2024-06-05 17:19:42 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+//  mongoose.connect("mongodb+srv://sourav:Sourav@123@mern-estate.ytbicfg.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to mongodb");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app=express();
 
 app.listen(3000,()=>{
