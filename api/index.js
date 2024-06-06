ff3fc6d70223dc731727a33d609d66c8d215c04d
Sourav <souravvaishyakiyar@gmail.com>
2024-06-06 18:56:16 +0530

    Created controller and api testing using postman

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 //  mongoose.connect("mongodb+srv://sourav:Sourav@123@mern-estate.ytbicfg.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
@@ -12,12 +14,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 const app=express();
+ app.use(express.json());
+
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!!!")
 });
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter)
 
 
 
