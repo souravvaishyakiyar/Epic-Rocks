0f75c6b6ae51b4c3310d9c3a27bff2267ca7fd12
Sourav <souravvaishyakiyar@gmail.com>
2024-06-06 16:38:09 +0530

    create usermodel and testapi

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 //  mongoose.connect("mongodb+srv://sourav:Sourav@123@mern-estate.ytbicfg.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
@@ -14,5 +15,9 @@ const app=express();
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!!!")
-})
+});
+
+app.use('/api/user',userRouter)
+
+
 
