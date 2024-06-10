a91139c02782dbb166efa0773dd40a3acbd12f6d
Sourav <souravvaishyakiyar@gmail.com>
2024-06-10 17:26:14 +0530

    Add and completed OAuth (Google Authorization)

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link,useNavigate} from "react-router-dom";
+import OAuth from "../Components/OAuth";
+// import OAuth from "../Components/OAuth";
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
@@ -69,6 +71,8 @@ export default function Signup() {
         >
           {loading ? "Loading...." : "SignUp"}
         </button>
+        <OAuth/>
+        
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnorhXbQllj-9mgwWns24WweOE1R8P9c4",
+  authDomain: "real-estate-6349a.firebaseapp.com",
+  projectId: "real-estate-6349a",
+  storageBucket: "real-estate-6349a.appspot.com",
+  messagingSenderId: "451869781951",
+  appId: "1:451869781951:web:a7ab94b52dde9402f27b0a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
