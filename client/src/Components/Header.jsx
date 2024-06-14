ae6c18493f8784467dd26b4d9f7ad44a359e0422
Sourav <souravvaishyakiyar@gmail.com>
2024-06-14 19:29:14 +0530

    Update page is functional

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
-  const currentUser  = useSelector(state => state.user);
+  const {currentUser}  = useSelector(state => state.user);
+  // console.log(currentUser.username)
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
