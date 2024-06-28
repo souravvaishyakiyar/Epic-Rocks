e01913d9e661ed70f0f5968988aea6523cd5f44d
Sourav <souravvaishyakiyar@gmail.com>
2024-06-28 15:59:05 +0530

    add preinstallation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,42 +1,38 @@
-import User from "../models/user.models.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import User from '../models/user.models.js';
+// import User from '../models/user.models.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User created succesfully");
+    res.status(201).json('User created successfully!');
   } catch (error) {
-    // res.status(500).json(error.message);
     next(error);
-    //  next(errorHandler(550,'Error from function'));
   }
-
-  // console.log(req.body);
 };
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User not found!"));
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
     next(error);
   }
 };
-
 
 export const google = async (req, res, next) => {
   try {
@@ -74,11 +70,11 @@ export const google = async (req, res, next) => {
   }
 };
 
-export const signOut=async(req,res,next)=>{
+export const signOut = async (req, res, next) => {
   try {
     res.clearCookie('access_token');
-    res.status(200).json('User has been logged out')
+    res.status(200).json('User has been logged out!');
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
